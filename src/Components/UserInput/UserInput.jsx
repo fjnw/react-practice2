758c7d246c1b1c6a4a8fcd8c104e3f8c3a8c51d4
Jamal <faizanjamal2018@u.northwestern.edu>
2018-12-18 19:00:32 -0600

    removed unused default code and images. added 2 components.

---
+++ src/Components/UserInput/UserInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userinput  = () => {
+
+  return (
+    <div className="UserInput">
+      <input />
+    </div>
+  )
+} 
+
+export default userinput; 
---
+++ src/Components/UserOutput/UserOutput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useroutput  = () => {
+
+  return (
+    <div className="UserOutput">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum fugiat inventore dignissimos tenetur! Modi nihil dolores incidunt possimus, natus numquam non esse repellat soluta quam dignissimos rerum maiores sed.</p>
+    </div>
+  )
+} 
+
+export default useroutput; 
